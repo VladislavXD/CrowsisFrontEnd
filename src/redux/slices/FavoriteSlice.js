b855708ab779960192240f73e6550a20da93ad69
Neivo <vladgamer1175@gmail.com>
2024-01-21 00:44:25 +0500

    add storage for favorites

--- src/redux/slices/FavoriteSlice.js
+++ src/redux/slices/FavoriteSlice.js
@@ -1,16 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+
+
+
+
 const favoriteSlice = createSlice({
   name: 'favorites',
   initialState: {
     list: JSON.parse(localStorage.getItem('favorites')) || [],
-  },
+
+    },
   reducers: {
     addToFavorites: (state, action) => {
       state.list = [...state.list, action.payload];
+      localStorage.setItem('favorites', JSON.stringify(state.list));
+      
     },
     removeFromFavorites: (state, action) => {
       state.list = state.list.filter(product => product.id !== action.payload);
+      localStorage.setItem('favorites', JSON.stringify(state.list));
+
+     
     },
   },
 });
