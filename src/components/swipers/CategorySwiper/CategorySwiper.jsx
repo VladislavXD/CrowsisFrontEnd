efa413b15fff3e75e9bd1c88350b668f818a2659
Neivo <vladgamer1175@gmail.com>
2024-05-03 00:11:14 +0500

    fix product card content

--- src/components/swipers/CategorySwiper/CategorySwiper.jsx
+++ src/components/swipers/CategorySwiper/CategorySwiper.jsx
@@ -7,40 +7,18 @@ import s from './categorySwiper.module.css';
 import { FreeMode } from 'swiper/modules';
 import CatalogList from '../../../DataListContent/Data.list.catalog';
 
+
+
+
+
 export default function CategorySwiper() {
-    const [resultScreen, setResultScreen] = useState(0);
 
-    useEffect(() => {
-        const handleResize = () => {
-            const width = window.innerWidth;
-            const screens = {
-                1120: 5.2,
-                920: 4.2,
-                720: 3.2,
-                520: 2.5,
-                420: 2
-            };
-            // Find the closest width match in the screens object
-            const closestWidth = Object.keys(screens).reduce((prev, curr) => {
-                return Math.abs(curr - width) < Math.abs(prev - width) ? curr : prev;
-
-            });
-            setResultScreen(screens[closestWidth]);
-        };
-
-        handleResize(); // Call initially
-        window.addEventListener("resize", handleResize);
-
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
 
     return (
         <Swiper
             style={{ height: '220px' }}
-            slidesPerView={resultScreen ?? 8}
-            spaceBetween={30}
+            watchSlidesProgress={true}
+             slidesPerView={7.7}
             freeMode={true}
             modules={[FreeMode]}
             className={s.mySwiper}
